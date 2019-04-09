8af22252062f0a0b672f4e8cb1c8016c321ef136
MasterTj <798750509@qq.com>
2019-04-09 21:11:28 +0800

    共享内存 MappedByteBuffer的使用方法。。。  2019/3/7 16:21

---
+++ communication-nio/src/main/java/com/basic/Test.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+/**
+ * locate com.basic
+ * Created by MasterTj on 2019/3/8.
+ */
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader br1=new BufferedReader(new InputStreamReader(new FileInputStream("")));
+        BufferedReader br2=new BufferedReader(new FileReader(new File("")));
+        BufferedInputStream bfi1=new BufferedInputStream(new FileInputStream(""));
+        DataInputStream dataInputStream=new DataInputStream(new BufferedInputStream(new FileInputStream("")));
+        byte[] bytes = "".getBytes();
+        ByteBuffer wrap = ByteBuffer.wrap(bytes);
+    }
+}
